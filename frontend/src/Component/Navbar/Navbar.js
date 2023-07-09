f5ffe18ccec9676e71751e1b1fd65a0083fbcc28
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-09 13:02:55 +0530

    committing navbar(2)

--- frontend/src/Component/Navbar/Navbar.js
+++ frontend/src/Component/Navbar/Navbar.js
@@ -1,9 +1,52 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    makeStyles,
+  } from "@material-ui/core";
+  import { useNavigate } from "react-router-dom";
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
+  const Navbar = (props) => {
+    const classes = useStyles();
+    let history = useNavigate();
+  
+    const handleClick = (location) => {
+      console.log(location);
+      history(location);
+    };
+  
+    return (
+      <AppBar position="fixed" style={{backgroundColor: 'green' }}>
+        <Toolbar>
+          <Typography variant="h5" className={classes.title} onClick={() => handleClick("/")}>
+            HELPER
+          </Typography>
+          
+        <>
+          <Button color="inherit" onClick={() => handleClick("/login")}>
+            Login
+          </Button>
+          <Button color="inherit" onClick={() => handleClick("/signup")}>
+            Signup
+          </Button>
+        </>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+  
+  export default Navbar;
+  
