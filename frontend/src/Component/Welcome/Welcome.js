1e47e5738e3970e3a040329b1b284f2bb9f9aefd
Poulami <mitrapoulamidgp@gmail.com>
2023-07-09 12:46:02 +0530

    committing welcome

--- frontend/src/Component/Welcome/Welcome.js
+++ frontend/src/Component/Welcome/Welcome.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import "./Welcome.css"
 
-const Welcome = () => {
+const Welcome = (props) => {
+  return (
+    <div className="flex-container">
+      <div className="body">
+        <h1><b>Welcome to Job Portal</b></h1>
+        <p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequatur ad nihil aliquam placeat recusandae labore libero itaque ipsum quibusdam sunt? Veritatis dolores id, aut totam aperiam dolor sequi sunt.</b></p>
+      </div>
+      <div className="image">
+        <img src='./images/maid.png' alt="image" />
+      </div>
+    </div>
+  );
+};
+
+export const ErrorPage = (props) => {
   return (
     <div>
-      
+      <div>
+        <h2>ERROR 404</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
+
