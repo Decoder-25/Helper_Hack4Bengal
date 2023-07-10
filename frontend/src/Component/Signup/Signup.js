1cff7f8ec466dd26a6b1cd5fc95c0e42567fc4d2
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-10 15:00:07 +0530

    n/a

--- frontend/src/Component/Signup/Signup.js
+++ frontend/src/Component/Signup/Signup.js
@@ -35,7 +35,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://localhost:8000/api/v1/auth/register", {
+      const response = await fetch("http://localhost:8000/api/v1/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
